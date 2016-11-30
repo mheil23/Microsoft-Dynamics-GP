41aab4b244f1c31d782579faf023396d6fac1bb7
Marc Heil <mheil23@users.noreply.github.com>
2016-11-30 16:34:26 -0500

    Update UserCleanup.sql

--- UserCleanup.sql
+++ UserCleanup.sql
@@ -3,17 +3,17 @@
 
 
 
-
-USE TESTA;
+--in this case the schema was actually the name of a user, I was trying to remove the views associated with junk from the user.
+USE <database>;
 DECLARE @output NVARCHAR(MAX) = N'';
 SELECT @output += CHAR(13) + CHAR(10) 
-  + 'DROP VIEW [ABS\ehelm2].[' + t.name + '];'
+  + 'DROP VIEW [<Schema>].[' + t.name + '];'
   FROM sys.views AS t
-  WHERE schema_id = schema_id('ABS\ehelm2')
+  WHERE schema_id = schema_id('<schema>')
 EXEC sp_executesql @output;
 
 
-select * from sys.tables where schema_id = schema_id('ABS\ehelm2')
+select * from sys.tables where schema_id = schema_id('<schema>')
 
 
 
